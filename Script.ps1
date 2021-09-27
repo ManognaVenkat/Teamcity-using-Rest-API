6beefdea33d5dfbeb6e07c473d46b051da2b7d33
ManognaVenkat <56588674+ManognaVenkat@users.noreply.github.com>
2021-09-27 17:56:24 +0530

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,10 @@
+$body = "<build><buildType id='<insert the buildid-value here>'/></build>"
+Write-Host "Trying to retrive data from Teamcity"
+$headers = @{
+        "Authorization"="Bearer <insert the token-value here >"
+        "Accept" = "application/json"
+        "Content-Type" = "application/xml"
+}
+$api = "https://<teamcity url>/app/rest/buildQueue"
+Invoke-RestMethod -Uri "https://<teamcity url>/app/rest/buildQueue" -Method Get -Headers $headers
+Invoke-RestMethod -Uri "https://<teamcity url>/app/rest/buildQueue" -Method 'Post' -Body $body -Headers $headers
