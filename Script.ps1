db488692f7f1653cf3b0c6f1f429a869d10aa2e9
ManognaVenkat <56588674+ManognaVenkat@users.noreply.github.com>
2021-10-01 13:12:10 +0530

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -3,7 +3,7 @@ Write-Host "Trying to retrive data from Teamcity"
 $headers = @{
         "Authorization"="Bearer <insert the token-value here >"
         "Accept" = "application/json"
-        "Content-Type" = "application/xml"
+        "Content-Type" = "application/json"
 }
 $api = "https://<teamcity url>/app/rest/buildQueue"
 Invoke-RestMethod -Uri "https://<teamcity url>/app/rest/buildQueue" -Method Get -Headers $headers
